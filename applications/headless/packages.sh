3a123a7c5c3aedb35ab576b784e1dcea1cd9490d
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2025-10-07 19:40:04 -0300

    adjusting packages for archlinux

---
+++ applications/headless/packages.sh
@@ -0,0 +1,35 @@
+packages=(
+  # Common tools
+  "wget"
+  "curl"
+  "net-tools"
+  "traceroute"
+  "iproute2"
+  "tldr"
+  "ncdu"
+  "unzip"
+  "openssh"
+  "man-db"
+  "bat"
+  "eza"
+  "coreutils"
+  "ripgrep"
+  "docker"
+  "fastfetch"
+
+  # Shell tools
+  "zsh-syntax-highlighting"
+  "zsh-autosuggestions"
+
+  # TUI's
+  "tmux"
+  "btop"
+  "vim"
+  "neovim"
+
+  # C/C++ tools (arch)
+  "base-devel"
+  "gcc-11"
+  "g++-11"
+  "make"
+)
