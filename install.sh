8cf22770d3ed38843158be00f0fb83c965a74e65
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2025-10-08 23:46:26 -0300

    changing file name

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOG_FILE="main.log"
+
+source ./applications/headless/packages.sh
+source ./utils.sh
+
+main() {
+  check_configure_git
+  install_packages
+  asdf_configure
+  clone_repositories
+  zsh_configurations
+  tmux_configurations
+  nvim_config
+  btop_configure
+  p10k_configuration
+
+  echo "Configuration complete. Run "source ~/.zshrc" and "source ~/.tmux.conf" to apply changes."
+}
+
+main 2>&1 | tee "$LOG_FILE"
