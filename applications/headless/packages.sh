4108a9148bd6523e7ca86c45b7e1eba760015633
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2025-10-08 23:45:22 -0300

    adding lazygit and lazydocker

--- applications/headless/packages.sh
+++ applications/headless/packages.sh
@@ -1,35 +1,38 @@
 packages=(
-  # Common tools
-  "wget"
-  "curl"
-  "net-tools"
-  "traceroute"
-  "iproute2"
-  "tldr"
-  "ncdu"
-  "unzip"
-  "openssh"
-  "man-db"
-  "bat"
-  "eza"
-  "coreutils"
-  "ripgrep"
-  "docker"
-  "fastfetch"
+    # Common tools
+    "wget"
+    "curl"
+    "net-tools"
+    "traceroute"
+    "iproute2"
+    "tldr"
+    "ncdu"
+    "unzip"
+    "openssh"
+    "man-db"
+    "bat"
+    "eza"
+    "coreutils"
+    "ripgrep"
+    "docker"
+    "fastfetch"
 
-  # Shell tools
-  "zsh-syntax-highlighting"
-  "zsh-autosuggestions"
+    # Shell tools
+    "zsh-syntax-highlighting"
+    "zsh-autosuggestions"
+    "zsh-completions"
 
-  # TUI's
-  "tmux"
-  "btop"
-  "vim"
-  "neovim"
+    # TUI's
+    "tmux"
+    "btop"
+    "vim"
+    "neovim"
+    "lazygit"
+    "lazydocker"
 
-  # C/C++ tools (arch)
-  "base-devel"
-  "gcc-11"
-  "g++-11"
-  "make"
+    # C/C++ tools (arch)
+    "base-devel"
+    "gcc-11"
+    "g++-11"
+    "make"
 )
