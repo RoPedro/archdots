72d2c382fe917e84d110c1021a89aba0a68cc06c
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2025-10-10 20:03:46 -0300

    wip: continue removing old code

--- install.sh
+++ install.sh
@@ -6,17 +6,15 @@ source ./applications/headless/packages.sh
 source ./utils.sh
 
 main() {
-  check_configure_git
-  install_packages
-  asdf_configure
-  clone_repositories
-  zsh_configurations
-  tmux_configurations
-  nvim_config
-  btop_configure
-  p10k_configuration
+    check_configure_git
+    install_yay
+    install_packages
 
-  echo "Configuration complete. Run "source ~/.zshrc" and "source ~/.tmux.conf" to apply changes."
+    sudo chsh -s $(which zsh) $USER
+
+    copy_dotfiles
+
+    echo "Configuration complete. Run "source ~/.zshrc" and "source ~/.tmux.conf" to apply changes."
 }
 
 main 2>&1 | tee "$LOG_FILE"
