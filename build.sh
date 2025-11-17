341ef8f42433b93e231415cc89a6a2cb90929dc1
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2025-11-17 20:19:44 -0300

    qol: adds .editorconfig and fixes incorrect identations

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ IMAGE_NAME="archtest"
 
 # Delete old image if it exists
 if docker image inspect "$IMAGE_NAME" >/dev/null 2>&1; then
-  echo "Removing existing image: $IMAGE_NAME"
-  docker rmi -f "$IMAGE_NAME"
+    echo "Removing existing image: $IMAGE_NAME"
+    docker rmi -f "$IMAGE_NAME"
 fi
 
 # Build new image without cache
 echo "Building new image: $IMAGE_NAME (no cache)"
 docker build --no-cache -t "$IMAGE_NAME" .
-
